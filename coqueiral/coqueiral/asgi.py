bb340cb9f3ecdb8a2fb860e74b2964d719ad4a49
Jean Lucas <jeanlucas2504@gmail.com>
2024-12-09 22:00:14 -0300

    reestruturação para usar microservices + postgres

--- coqueiral/coqueiral/asgi.py
+++ coqueiral/coqueiral/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for devweb2 project.
+ASGI config for coqueiral project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devweb2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coqueiral.settings')
 
 application = get_asgi_application()
--- coqueiral/coqueiral/urls.py
+++ coqueiral/coqueiral/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for devweb2 project.
+URL configuration for coqueiral project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -19,5 +19,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('coqueiral.urls')),
 ]
